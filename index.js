4d200f41a0856baa67b721c1cf75b11f60ff144b
Olexandr Galutskiy <kniazgal@gmail.com>
2018-03-30 18:41:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+
+//import App from "./src/scenes/loginScene";
+import App from "./src/scenes/welcomeScene";
+
+
+AppRegistry.registerComponent('App', () => App);
---
+++ src/scenes/welcomeScene.js
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native";
+import React from "react";
+import Layout from "../components/layout"
+
+const welcomeScene = ()=> (
+  <Layout>
+    <Text>123</Text>
+
+  </Layout>
+)
+
+export default welcomeScene;
